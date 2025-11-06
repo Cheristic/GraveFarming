fd5e414cf8991a7476585b42a187847d48f84ef5
Cheristic <ethanheffan@gmail.com>
2025-11-06 12:27:43 +0100

    changes round manager to use coroutines

--- Assets/Managers/RoundManager.cs
+++ Assets/Managers/RoundManager.cs
@@ -2,47 +2,53 @@ using System;
 using UnityEngine;
 using TMPro;
 using Unity.VisualScripting;
+using System.Collections;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class RoundManager : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI timerText;
     [SerializeField] float restTime = 30.0f;
-    private float activeTimeRemaining;
-    [SerializeField] bool roundActive = true;
     [SerializeField] int roundNum = 1;
+
+    bool roundActive = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        activeTimeRemaining = restTime;
+        roundActive = false;
+        // start rest period
+        StartCoroutine(RestPeriodTimer());
     }
 
     // Update is called once per frame
     void Update()
     {
         // This causes issues with the Input System Package
-        //if (Input.GetKeyDown(KeyCode.P))
+        if (Input.GetKeyDown(KeyCode.P))
         {
-          //  EndActivePhase();
+            if (roundActive) EndActivePhase();
+            else BeginActivePhase();
         }
+    }
 
-        if (activeTimeRemaining <= 0.0f)
+    IEnumerator RestPeriodTimer()
+    {
+        float timeRemaining = restTime;
+        while (timeRemaining > 0)
         {
-            BeginActivePhase();
-        }
-
-        if (!roundActive)
-        {
-            activeTimeRemaining -= Time.deltaTime;
-            if (activeTimeRemaining < 0) activeTimeRemaining = 0.0f;
-            int minutes = Mathf.FloorToInt(activeTimeRemaining / 60);
-            int seconds = Mathf.FloorToInt(activeTimeRemaining % 60);
+            int minutes = Mathf.FloorToInt(timeRemaining / 60);
+            int seconds = Mathf.FloorToInt(timeRemaining % 60);
             timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
-        } 
+            yield return null; // this waits until next frame
+            timeRemaining -= Time.deltaTime;
+        }
+
+        BeginActivePhase();
     }
 
     public void BeginActivePhase()
     {
+        StopAllCoroutines(); // stops the above coroutine if active
         roundActive = true;
     }
     public void EndActivePhase()
@@ -50,9 +56,9 @@ public class NewMonoBehaviourScript : MonoBehaviour
         ++roundNum;
         roundActive = false;
 
-        activeTimeRemaining = restTime;
-
         // Debugging
         Debug.Log("End");
+
+        StartCoroutine(RestPeriodTimer());
     }
 }
