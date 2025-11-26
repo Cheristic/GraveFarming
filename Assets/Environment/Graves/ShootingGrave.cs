a55d4c1df983f20e31924057432d17e01551f2bc
Cheristic <ethanheffan@gmail.com>
2025-11-26 21:35:27 +0100

    updates Shooter priority behavior

--- Assets/Environment/Graves/ShootingGrave.cs
+++ Assets/Environment/Graves/ShootingGrave.cs
@@ -54,36 +54,56 @@ public class ShooterGrave : Grave
 
     IEnumerator ShooterBehaviorLoop()
     {
+
         while (true)
         {
-            Collider2D[] hits;
-            Vector3 playerPos;
-            while (true) {
-                playerPos = PlayerManager.Instance.transform.position;
-                hits = Physics2D.OverlapCircleAll(playerPos, PLAYER_CHECK_FOR_ENEMY_RADIUS, ENEMIES_LAYER);
-                if (hits.Length > 0) break;
-                yield return new WaitForSeconds(CHECK_FOR_ENEMY_INTERVAL);
-            }
-            Collider2D closestHit = null;
-            float closestDistance = Mathf.Infinity;
-            foreach (var hit in hits)
+            Collider2D target = null;
+            Vector2 playerPos = PlayerManager.Instance.transform.position;
+            Collider2D[] near_player = Physics2D.OverlapCircleAll(playerPos, PLAYER_CHECK_FOR_ENEMY_RADIUS, ENEMIES_LAYER);
+            if (near_player.Length > 0) // there are enemies around player
             {
-                float dist = Vector3.Distance(playerPos, hit.transform.position);
-                if (dist < closestDistance && dist < SHOOTING_RADIUS) { 
-                    closestHit = hit;  
-                    closestDistance = dist;
+                Collider2D closestHit = null;
+                float closestDistance = Mathf.Infinity;
+                foreach (var hit in near_player)
+                {
+                    float dist_to_player = Vector2.Distance(playerPos, hit.transform.position);
+                    float dist_to_grave = Vector2.Distance(transform.position, hit.transform.position);
+                    if (dist_to_player < closestDistance && dist_to_grave < SHOOTING_RADIUS)
+                    {
+                        closestHit = hit;
+                        closestDistance = dist_to_player;
+                    }
                 }
-            }
-            if (closestHit == null)
+                if (closestHit != null) target = closestHit;
+            } 
+            if (target == null) // either no enemies near player or none within range
             {
-                yield return new WaitForSeconds(CHECK_FOR_ENEMY_INTERVAL);
-                continue; // All outside shooting radius
+                Collider2D[] near_grave = Physics2D.OverlapCircleAll(transform.position, SHOOTING_RADIUS, ENEMIES_LAYER);
+                Collider2D closestHit = null;
+                float closestDistance = Mathf.Infinity;
+                foreach (var hit in near_grave)
+                {
+                    float dist = Vector2.Distance(transform.position, hit.transform.position);
+                    if (dist < closestDistance)
+                    {
+                        closestHit = hit;
+                        closestDistance = dist;
+                    }
+                }
+                if (closestHit != null) target = closestHit;
             }
 
+            if (target == null) // No target within range
+            {
+                yield return new WaitForSeconds(CHECK_FOR_ENEMY_INTERVAL);
+                continue; 
+            }
+
+            // SHOOT
             for (int i = 0; i < BULLETS_TO_SHOOT; i++) 
             {
                 var bullet = FetchBulletFromPool();
-                bullet.Shoot(gameObject, gameObject.transform.position, closestHit.transform.position);
+                bullet.Shoot(gameObject, gameObject.transform.position, target.transform.position);
 
                 if (i == BULLETS_TO_SHOOT - 1) yield return new WaitForSeconds(REST_TIME_AFTER_BULLETS);
                 else yield return new WaitForSeconds(TIME_BETWEEN_BULLETS);
@@ -94,8 +114,14 @@ public class ShooterGrave : Grave
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.blue;
-        Gizmos.DrawSphere(PlayerManager.Instance.transform.position, PLAYER_CHECK_FOR_ENEMY_RADIUS);
+        Color col = Gizmos.color;
+        col = new Color(col.r, col.g, col.b, .2f);
+        Gizmos.color = col;
+        Gizmos.DrawSphere((Vector2)PlayerManager.Instance.transform.position, PLAYER_CHECK_FOR_ENEMY_RADIUS);
         Gizmos.color = Color.yellow;
-        Gizmos.DrawSphere(transform.position, SHOOTING_RADIUS);
+        col = new Color(col.r, col.g, col.b, .2f);
+        Gizmos.color = col;
+
+        Gizmos.DrawSphere((Vector2)transform.position, SHOOTING_RADIUS);
     }
 }
