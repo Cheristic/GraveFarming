d26dcadd0e180442d8cd7b89acd0b54b59b8a4bf
Cheristic <ethanheffan@gmail.com>
2026-02-17 23:11:15 +0100

    camera follow

--- Assets/Camera/CameraFollower.cs
+++ Assets/Camera/CameraFollower.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class CameraFollower : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] Transform player;
+    [SerializeField] float threshold;
+    [SerializeField] float distanceLimiter;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // #### ROTATION ####
+        Vector2 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(player.position);
+
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector3 targetPos = dir.normalized * (Vector2.Distance(mousePos, player.position) / distanceLimiter);
+        targetPos = Vector3.ClampMagnitude(targetPos, threshold) + player.position;
+        targetPos.z = transform.position.z;
+
+        transform.position = targetPos;
     }
 }
